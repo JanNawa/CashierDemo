329fa748c37d1858b5e837ab9f025cfabe78f5a9
JanNawa <49848352+JanNawa@users.noreply.github.com>
2019-05-29 15:48:21 -0300

    Add Signup Messages

--- src/utils/AlertMessages.java
+++ src/utils/AlertMessages.java
@@ -10,6 +10,12 @@ public enum AlertMessages {
     ORDER_STATUS_NO_SELECTION_DESC("Please select product to delete product."),
     ORDER_STATUS_EMPTY_DESC("Your order is successfully confirmed!"),
     
+    SIGNUP_INVALID_USERNAME_TITLE("Invalid Username"),
+    SIGNUP_INVALID_USERNAME_DESC("This username has already used. Try Again!"),
+    
+    SIGNUP_DIFFERENT_PASSWORD_TITLE("Different Passwords"),
+    SIGNUP_DIFFERENT_PASSWORD_DESC("The passwords need to be the same. Try Again!"),
+    
     LOGIN_INVALID_TITLE("Invalid Account"),
     LOGIN_INVALID_DESC("The username or password is invalid."),
     
