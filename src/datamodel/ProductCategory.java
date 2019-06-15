78be9fc6d7e5948cb6af1f4efb15d2986439b386
JanNawa <49848352+JanNawa@users.noreply.github.com>
2019-06-15 11:35:01 -0300

    Create ProductCategory.java

---
+++ src/datamodel/ProductCategory.java
@@ -0,0 +1,17 @@
+package datamodel;
+
+/**
+ *
+ * @author Jan
+ */
+public class ProductCategory {
+    private int id;
+    private String name;
+    private String description;
+
+    public ProductCategory(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+}
