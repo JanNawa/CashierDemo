c09219c445ee0646b2837f9f091f4e9697246eb6
JanNawa <49848352+JanNawa@users.noreply.github.com>
2019-06-18 16:21:44 -0300

    Change category to listview

--- src/controller/ProductListController.java
+++ src/controller/ProductListController.java
@@ -1,11 +1,15 @@
 package controller;
 
 import datamodel.Product;
+import datamodel.ProductCategory;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -26,8 +30,13 @@ import javafx.stage.Stage;
 public class ProductListController implements Initializable {
     
     Product[] products;
-    
     List<Product> selectedProducts;
+    
+    ArrayList<ProductCategory> categories;
+    ListProperty<ProductCategory> categoryListProperty;
+    
+    @FXML
+    private ListView categoryListView;
 
     @FXML
     private Button product1Button;
@@ -61,17 +70,33 @@ public class ProductListController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         products = new Product[5];
-        products[0] = new Product(101, "Milk", "Low Fat 2.0 litres", 4.99);
-        products[1] = new Product(102, "Eggs", "White Large 12 Eggs", 2.49);
-        products[2] = new Product(103, "Bread", "100% Whole Wheat Sliced Bread", 2.00);
-        products[3] = new Product(104, "Yogurt", "Natural Yogurt 750 g", 2.89);
-        products[4] = new Product(105, "Salmon", "Atlantic Salmon Portion 300 g", 11.99);
+        products[0] = new Product(101, 111, "Milk", "Low Fat 2.0 litres", 4.99);
+        products[1] = new Product(102, 111, "Eggs", "White Large 12 Eggs", 2.49);
+        products[2] = new Product(103, 111, "Bread", "100% Whole Wheat Sliced Bread", 2.00);
+        products[3] = new Product(104, 111, "Yogurt", "Natural Yogurt 750 g", 2.89);
+        products[4] = new Product(105, 111, "Salmon", "Atlantic Salmon Portion 300 g", 11.99);
 
         product1Button.setText(products[0].getName());
         product2Button.setText(products[1].getName());
         product3Button.setText(products[2].getName());
         product4Button.setText(products[3].getName());
         product5Button.setText(products[4].getName());
+        
+        categories = new ArrayList<>();
+        categories.add(new ProductCategory(1, "Bakery", ""));
+        categories.add(new ProductCategory(2, "Dairy & Eggs", ""));
+        categories.add(new ProductCategory(3, "Pasta & Rice", ""));
+        categories.add(new ProductCategory(4, "Chips & Snacks", ""));
+        categories.add(new ProductCategory(5, "Frozen Food", ""));
+        categories.add(new ProductCategory(6, "Cereal & Breakfast", ""));
+        categories.add(new ProductCategory(7, "Drinks", ""));
+        categories.add(new ProductCategory(8, "Fruits & Vegetables", ""));
+        categories.add(new ProductCategory(9, "Meat & Seafood", ""));
+        
+        categoryListProperty = new SimpleListProperty<>();
+        categoryListProperty.set(FXCollections.observableArrayList(categories));
+        categoryListView.itemsProperty().bind(categoryListProperty);
+        categoryListView.setPrefWidth(130);
 
         selectedProducts = new ArrayList<>();
 
