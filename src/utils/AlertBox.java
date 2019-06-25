155f7220473885674039a0d99465a2c443eb345b
JanNawa <49848352+JanNawa@users.noreply.github.com>
2019-06-25 11:50:40 -0300

    Refactor AlertBox to static

--- src/utils/AlertBox.java
+++ src/utils/AlertBox.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Alert;
  * @author Jan
  */
 public class AlertBox {
-    public void showAlertBox(Alert.AlertType alertType, String title, String content){
+    public static void showAlertBox(Alert.AlertType alertType, String title, String content){
         Alert alert = new Alert(alertType);
             alert.setTitle(title);
             alert.setHeaderText(null);
