c71fdad651b889221e796dbf7f99b7dc28dc52eb
JanNawa <49848352+JanNawa@users.noreply.github.com>
2019-07-03 11:19:29 -0300

    Update SignupController.java

--- src/controller/SignupController.java
+++ src/controller/SignupController.java
@@ -2,14 +2,10 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.beans.value.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
 import utils.Validator.PasswordValidator;
 
 /**
