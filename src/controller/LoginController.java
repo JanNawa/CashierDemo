97c504092c79fcfb8d8a08c045b40211105c503f
JanNawa <49848352+JanNawa@users.noreply.github.com>
2019-07-03 11:20:03 -0300

    Update LoginController.java

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -2,10 +2,8 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.*;
+import javafx.scene.control.*;
 
 /**
  * FXML Controller class
